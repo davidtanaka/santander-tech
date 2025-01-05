381ceccd811202f258cc27e041e75e6c3a915320
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-01-05 13:41:41 -0300

    Adicionando alguns comandos no (arquivo.js)

--- curso-backend/modulo-io/aula2/arquivos.js
+++ curso-backend/modulo-io/aula2/arquivos.js
@@ -1,13 +1,18 @@
 const path = require('node:path');
 const fs = require('node:fs')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const filePath = path.join(process.cwd(), 'curso-backend', 'aula2', 'texto.txt')
-const fileOutPath = path.join(process.cwd(), 'curso-backend', 'aula2', 'texto-com-linhas.txt')
+console.log(process.env.NODE_ENV)
+
+const filePath = path.join(process.cwd(), 'curso-backend', 'modulo-io', 'aula2', 'texto.txt')
+const fileOutPath = path.join(process.cwd(), 'curso-backend', 'modulo-io', 'aula2', 'texto-com-linhas.txt')
 console.log(filePath);
 
+console.time('Manipular arquivos')
 fs.readFile(filePath, {}, (erro, dados) => {
     if (erro) {
-        console.log(`Erro na leitura do arquivo no caminho ${filePath}`)
+        console.error(`Erro na leitura do arquivo no caminho ${filePath}`)
         return
     }
     const texto = dados.toString()
@@ -19,9 +24,11 @@ fs.readFile(filePath, {}, (erro, dados) => {
 
     fs.writeFile(fileOutPath, linhasAjustadas.join('\n'), {}, (erro) => {
         if (erro) {
-            console.log(`Erro na leitura do arquivo no caminho ${filePath}`)
+            console.error(`Erro na leitura do arquivo no caminho ${filePath}`)
             return
         }
+        console.log(`Arquivo salvo no bucker ${process.env.S3_BUCKET}`)
+    console.timeEnd('Manipular arquivos')
     })
 })
 
